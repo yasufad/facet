6b0f3de871b442904e4b3fb445e4710f79ea498f
Yasu <yasufad@proton.me>
2026-08-31 04:46:51 +0300

    feat(examples/quad): use window and element frame loop instead of manual scene

--- examples/quad/main.go
+++ examples/quad/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"log"
 
+	"github.com/yasufad/facet/app"
 	"github.com/yasufad/facet/colour"
+	"github.com/yasufad/facet/element"
 	"github.com/yasufad/facet/geometry"
 	"github.com/yasufad/facet/platform"
-	"github.com/yasufad/facet/render"
-	"github.com/yasufad/facet/render/d3d11"
-	"github.com/yasufad/facet/scene"
+	"github.com/yasufad/facet/style"
+	"github.com/yasufad/facet/window"
 )
 
 func main() {
@@ -16,35 +17,50 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	w, err := p.NewWindow(platform.WindowOptions{Title: "Facet Quad", Size: geometry.NewSize[geometry.Pixels](640, 480), Resizable: true, Decorated: true, Visible: true})
-	if err != nil {
-		log.Fatal(err)
-	}
-	sf := w.ScaleFactor()
-	r, err := d3d11.New(w.NativeSurface(), geometry.NewSize(geometry.Pixels(640).ToDevicePixels(sf), geometry.Pixels(480).ToDevicePixels(sf)), render.Options{VSync: true})
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer r.Close()
-	draw := func() {
-		sc, s := w.ScaleFactor(), scene.New()
-		s.InsertQuad(scene.Quad{
-			Bounds:       geometry.NewBounds(geometry.NewPoint(geometry.ScaledPixels(100*sc), geometry.ScaledPixels(80*sc)), geometry.NewSize(geometry.ScaledPixels(440*sc), geometry.ScaledPixels(320*sc))),
-			Background:   colour.Rgba{R: 0.15, G: 0.5, B: 0.85, A: 1.0},
-			BorderColour: colour.Rgba{R: 0.95, G: 0.75, B: 0.2, A: 1.0},
-			CornerRadii:  geometry.AllCorners(geometry.ScaledPixels(24 * sc)),
-			BorderWidths: geometry.AllEdges(geometry.ScaledPixels(4 * sc)),
-		})
-		s.Finish()
-		_ = r.Draw(s)
-		_ = r.Present()
-	}
-	w.SetEventHandler(func(e platform.Event) {
-		if _, ok := e.(platform.ResizeEvent); ok {
-			_ = r.Resize(geometry.NewSize(w.Size().Width.ToDevicePixels(w.ScaleFactor()), w.Size().Height.ToDevicePixels(w.ScaleFactor())))
-			draw()
-		}
+
+	a := app.NewApp()
+	defer a.Close()
+
+	w, err := window.New(p, a, window.WindowOptions{
+		Title:     "Facet Quad",
+		Size:      geometry.NewSize[geometry.Pixels](640, 480),
+		Resizable: true,
+		Decorated: true,
+		Visible:   true,
+		VSync:     true,
 	})
-	p.Dispatch(draw)
-	_ = p.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer w.Close()
+
+	bgColour := colour.Rgba{R: 0.15, G: 0.5, B: 0.85, A: 1.0}
+	hoverBgColour := colour.Rgba{R: 0.25, G: 0.65, B: 0.95, A: 1.0}
+	borderColour := colour.Rgba{R: 0.95, G: 0.75, B: 0.2, A: 1.0}
+
+	w.SetRootFn(func() element.Element {
+		return element.NewDiv().
+			Flex().
+			WFull().
+			HFull().
+			ItemsCenter().
+			JustifyCenter().
+			Child(
+				element.NewDiv().
+					Width(style.Px(440)).
+					Height(style.Px(320)).
+					Bg(bgColour).
+					BorderColour(borderColour).
+					Border(geometry.Pixels(4)).
+					Rounded(geometry.Pixels(24)).
+					Hover(func(s *style.Refinement) {
+						s.SetBackground(hoverBgColour)
+					}),
+			)
+	})
+
+	p.Dispatch(w.Draw)
+	if err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
